05922e6358cb19bbbaece6bea2c82d0341d2fee7
Guilherme Bittelbrunn <guilherme.bitte@unifebe.edu.br>
2023-02-15 15:34:27 -0300

    Interação com imagens
    Interação com imagem utilizando o módulo npm Sharp
    -Redimensionando uma imagem
    -Rotacionadno
    -Criando uma nova imagem

--- Node JS/15-02-2023/Imagem/main.js
+++ Node JS/15-02-2023/Imagem/main.js
@@ -1,20 +1,46 @@
+const sharp = require('sharp');
+const compress_images = require('compress-images');
+
 let path = process.argv[2];
 let width = parseInt(process.argv[3]);
 
-const sharp = require('sharp');
+function resize(inputPath, outputPath, width) {
 
-
-function resize(path, width){
-
-    sharp(path).resize({width: width}).toFile('./testandoimagem.jpg', (error)=>{
-        if(error){
+    sharp(inputPath).resize({ width: width }).toFile(outputPath, (error) => {
+        if (error) {
             console.log(error);
         }
-        else{
-            console.log("Imagem redimencionada com sucesso");
+        else {
+            console.log("Imagem redimensionada com sucesso");
+            // compress(outputPath, './compress/');
+            rotacionar(outputPath, 90);
         }
     });
 }
 
+function rotacionar(outputPath, angle){
+    sharp(outputPath).rotate(angle).toFile("imagemrotacionada.jpg", (err)=>{
+        if(err){console.log(err)}
+        else{console.log("Imagem rotacionada")}
+    })
+}
 
-resize(path, width);
+// function compress(pathInput, pathOutpath) {
+//     compress_images(pathInput, pathOutpath, { compress_force: false, statistic: true, autoupdate: true }, false,
+//         { jpg: { engine: "mozjpeg", command: ["-quality", "60"] } },
+//         { png: { engine: "pngquant", command: ["--quality=20-50", "-o"] } },
+//         { svg: { engine: "svgo", command: "--multipass" } },
+//         { gif: { engine: "gifsicle", command: ["--colors", "64", "--use-col=web"] } },
+//         function (error, completed, statistic) {
+//             console.log("-------------");
+//             console.log(error);
+//             console.log(completed);
+//             console.log(statistic);
+//             console.log("-------------");
+//         }
+//     );
+// }
+
+
+resize(path, 'novaimagem.jpg', width);
+
