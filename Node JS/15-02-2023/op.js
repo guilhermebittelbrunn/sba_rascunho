0c5f7eb4eaca1389f9840e2efda1d692cfa45104
Guilherme Bittelbrunn <guilherme.bitte@unifebe.edu.br>
2023-02-15 11:07:49 -0300

    Adicao de pacotes
    Adicionando e testando pacotes

---
+++ Node JS/15-02-2023/op.js
@@ -0,0 +1,8 @@
+let operacao = {
+    soma: (valor1,valor2)=>{return valor1 + valor2},
+    sub: (valor1,valor2)=>{return valor1 - valor2},
+    mult: (valor1,valor2)=>{return valor1 * valor2},
+    div: (valor1,valor2)=>{return valor1 / valor2},
+}
+
+module.exports = operacao;
---
+++ Node JS/15-02-2023/script.js
@@ -0,0 +1,6 @@
+let op = require('./op');
+let calc = require('./areas');
+
+
+console.log(op.mult(3,4));
+console.log(calc.area_quadrado(4));
