66c2ff2c40d0b23f6ed6f0e9fde4177a99945a4c
Guilherme Bittelbrunn <guilherme.bitte@unifebe.edu.br>
2023-02-23 11:20:16 -0300

    revisao

--- Node JS/23-02-2323/script1/index.js
+++ Node JS/23-02-2323/script1/index.js
@@ -1,49 +1,32 @@
-const ejs = require("ejs");
-const express = require("express");
-const fs = require('fs');
+const express = require('express');
+const ejs = require('ejs');
 const path = require('path');
+
+const PORT = 4000;
 const app = express();
-const PORT = 3000;
+const pathfile = path.join(__dirname, 'views');
 
 let alunos = [
-    { nome: "Gustavo",  turma: "5A" },
-    { nome: "Jessica",  turma: "3B" },
-    { nome: "Douglas",  turma: "2A" }
+    {Nome: 'Jessica', Turma: '5B'},
+    {Nome: 'Guilherme', Turma: '3A'},
+    {Nome: 'Camilly', Turma: '7C'}
 ]
-app.set('views', './views');
-app.set('view engine', "ejs");
 
-app.get('/' ,(req,res)=>{
-    res.render('index', {alunos});
+app.set('views', pathfile);
+app.set('view engine', 'ejs');
+
+app.get('/', (req,res)=>{
+    res.render('index',{alunos:alunos});
 })
 
-app.get('/about', (req,res)=>{
-    res.render('about')
+app.get('/sobre',(req,res)=>{
+    res.render('about');
 })
-
 
 app.listen(PORT, (err)=>{
     if(err){
         console.log(err);
     }else{
-        console.log('Servidor rodando na porta:' + PORT);
+        console.log('Servidor rodando na porta: ' + PORT);
     }
 })
-
-// //FORMA 1
-// for (let aluno = 0; aluno < alunos.length; aluno++){
-//     console.log(alunos[aluno].turma);
-// }
-
-// //FORMA 2
-// alunos.forEach((aluno)=>{
-//     console.log(aluno.nome[0]);
-//     console.log(aluno.turma);
-// })
-
-// //FORMA 3
-// for (let aluno of alunos){
-//     console.log(aluno.nome);
-//     console.log(aluno.turma);
-// }
-
