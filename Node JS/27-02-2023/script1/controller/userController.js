a82e7e725b9189c2012343caa3714f13e96c9e3a
Guilherme Bittelbrunn <guilherme.bitte@unifebe.edu.br>
2023-02-27 17:25:41 -0300

    add bcrypt functions
    Registrando com senha cryptografada
    
    falta fazer a autenticação de login

--- Node JS/27-02-2023/script1/controller/userController.js
+++ Node JS/27-02-2023/script1/controller/userController.js
@@ -1,13 +1,20 @@
 const User = require('../module/User');
+const bcrypt = require('bcryptjs');
 
 module.exports = {
 
     register: async function (req,res){
 
+        const newUser = await User.findOne({email: req.body.email});
+        
+        if(newUser){
+            return res.status(400).send(`E-mail: ${req.body.email} já está em uso`);
+        }
+
         const user = new User({
             nome: req.body.nome,
             email: req.body.email,
-            password: req.body.password
+            password: bcrypt.hashSync(req.body.password)
         })
         try{
             let saveUser = await user.save();
@@ -16,8 +23,17 @@ module.exports = {
             console.log(err);
         }
     },
-    login: function(req,res){
-        res.send('login')
+    login: async function(req,res){
+        const login = await User.findOne({email: req.body.email});
+        if(!login){
+            res.status(400).send('E-mail ou senha inválido');
+        }else{
+            let SenhaEmailMatch = bcrypt.compareSync(req.body.password == login.password);
+            if(!SenhaEmailMatch){
+                return res.status(400).send('E-mail ou senha inválido');
+            }
+            res.send(`Bem-vindo ${login.nome}`);
+        }
     },
 
     registerPage: function(req,res){
