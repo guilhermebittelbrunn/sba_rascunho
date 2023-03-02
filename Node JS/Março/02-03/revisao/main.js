c4dcaccf74c35a460f16b2250586a3a561ead5b6
Guilherme Bittelbrunn <guilherme.bitte@unifebe.edu.br>
2023-03-02 09:03:49 -0300

    Create resumo 02-03

---
+++ Node JS/Março/02-03/revisao/main.js
@@ -0,0 +1,22 @@
+// const Carro = {
+//     marca: 'Volkswagen',
+//     modelo: 'Jetta',
+//     ano: 2018
+// }
+
+// const {modelo, ano} = Carro;
+
+// console.log(modelo + ano);
+
+
+const array1 = [10,20,30];
+const array2 = [...array1, 50, 60];
+const [primeiro,segundo] = array2
+
+console.log(array2);
+array1.push(40);
+console.log(array2);
+console.log(array1);
+console.log(primeiro);
+console.log(segundo);
+
