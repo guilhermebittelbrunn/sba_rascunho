3ced203ee215f3fa1565d2764d03fb73f22fccec
Guilherme Bittelbrunn <guilherme.bitte@unifebe.edu.br>
2023-03-03 17:19:10 -0300

    Add group function
    Adição da funcionalidade de grupos, assim privando mensagens em cada chat

--- Node JS/Desafios/Desafio 4/public/chat.js
+++ Node JS/Desafios/Desafio 4/public/chat.js
@@ -1,4 +1,5 @@
-const socket = io('http://localhost:3000');
+const room = window.location.pathname.replace(/\//g, '');
+const socket = io('http://localhost:3000/' + room);
 let user = null;
 
 socket.on('updateMessages', (messages)=>{
@@ -16,23 +17,22 @@ function updateScreen(messages){
 }
 
 document.addEventListener('DOMContentLoaded', ()=>{
-    if(user){
-        alert('Identifique-se');
-        return;
-    }
     const form_user = document.getElementById('form_user');
+    form_messages.addEventListener('submit', (event)=>{
+        event.preventDefault();
+        if(!user){
+            alert('Identifique-se');
+            return;
+        }
+        const form_input_value = document.forms['form_messages']['msg'].value;
+        document.forms['form_messages']['msg'].value = '';
+        console.log(user,  form_input_value)
+        socket.emit('newMessage', {user: user, msg: form_input_value});
+    })
     form_user.addEventListener('submit', (event)=>{
         event.preventDefault();
         user = document.forms['form_user_name']['user'].value;
         document.forms['form_user_name']['user'].value = '';
         form_user.style.display = 'none';
     })
-    form_messages.addEventListener('submit', (event)=>{
-        event.preventDefault();
-        const form_input_value = document.forms['form_messages']['msg'].value;
-        document.forms['form_messages']['msg'].value = '';
-        console.log(user,  form_input_value)
-        socket.emit('newMessage', {user: user, msg: form_input_value});
-    })
-    
 })
--- Node JS/Desafios/Desafio 4/script.js
+++ Node JS/Desafios/Desafio 4/script.js
@@ -5,9 +5,11 @@ const path = require('path');
 const port = 3000;
 const app = express();
 const path_index = path.join(__dirname, 'public');
-const messages = [];
+const messages = {grupo1: [],grupo2:[]};
 
-app.use('/',express.static(path_index));
+app.use('/grupo1',express.static(path_index));
+app.use('/grupo2',express.static(path_index));
+
 const server = app.listen(port, (err)=>{
     if(err) console.log(err);
     else console.log('Server running on port: ' + port);
@@ -15,17 +17,29 @@ const server = app.listen(port, (err)=>{
 
 const io = socketIO(server);
 
-io.on('connection', (socket)=>{
+const grupo1 = io.of('/grupo1').on('connection', (socket)=>{
     
     console.log('new connection');
-    socket.emit('updateMessages', messages);
+    socket.emit('updateMessages', messages.grupo1);
 
     socket.on('newMessage', (message)=>{
 
-        messages.push(message);
-        io.emit('updateMessages', messages);
+        messages.grupo1.push(message);
+        grupo1.emit('updateMessages', messages.grupo1);
+
+    }) 
+})
+
+const grupo2 = io.of('/grupo2').on('connection', (socket)=>{
+    
+    console.log('new connection');
+    socket.emit('updateMessages', messages.grupo2);
+
+    socket.on('newMessage', (message)=>{
+
+        messages.grupo2.push(message);
+        grupo2.emit('updateMessages', messages.grupo2);
 
     })
     
-
 })
