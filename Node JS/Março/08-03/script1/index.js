7d67fb7a834a27bc92d7acddc38bc040e220b426
Guilherme Bittelbrunn <guilherme.bitte@unifebe.edu.br>
2023-03-08 17:23:17 -0300

    add 08-03 script
    script1 and revisao.txt

---
+++ Node JS/Março/08-03/script1/index.js
@@ -0,0 +1,56 @@
+const arr = [
+    {
+        tipo: 'a',
+        qtd: 1,
+    },
+    {
+        tipo: 'b',
+        qtd: 2,
+    },
+    {
+        tipo: 'a',
+        qtd: 3,
+    },
+    {
+        tipo: 'b',
+        qtd: 5,
+    },
+    {
+        tipo: 'c',
+        qtd: 3,
+    },
+];
+
+const obj = arr.reduce(
+    (obj, item) => {
+        // !obj[item.tipo] &&
+        //     (obj[item.tipo] = {
+        //         total: 0,
+        //     });
+
+        // if (!obj[item.tipo]) {
+        //      obj[item.tipo] = {
+        //          total: 0,
+        //      };
+        //  }
+
+        obj[item.tipo] = obj[item.tipo] || {
+            //?? Nullish coalescing operator (??)
+            total: 0,
+        };
+
+        obj[item.tipo].total += item.qtd;
+
+        return obj;
+    },
+    {
+        // a: {
+        //     total: 0,
+        // },
+        // b: {
+        //     total: 0,
+        // },
+    }
+);
+
+console.log(obj);
