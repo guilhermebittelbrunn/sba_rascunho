4ef28c8da05409d41146e55c401ce3939666ee6b
Guilherme Bittelbrunn <guilherme.bitte@unifebe.edu.br>
2023-03-13 11:54:46 -0300

    Add revisao and script1
    Finalizando os conceitos de reduce,
    utilizando objetos, listas e strings

---
+++ Node JS/Março/13-03/revisao/revisao.js
@@ -0,0 +1,113 @@
+let Produtos = [
+    {
+        titulo: 'Leite',
+        preco: 5.00,
+        categoria: 'alimento',
+        status: {
+            estoque: true,
+            ativo: true
+        }
+    },
+    {
+        titulo: 'Banana',
+        preco: 3.00,
+        categoria: 'alimento',
+        status: {
+            estoque: true,
+            ativo: true
+        }
+    },
+    {
+        titulo: 'Bolacha',
+        preco: 3.00,
+        categoria: 'alimento',
+        status: {
+            estoque: false,
+            ativo: true
+        }
+    },
+    {
+        titulo: 'Papel-higiênico',
+        preco: 12.00,
+        categoria: 'higiene',
+        status: {
+            estoque: true,
+            ativo: true
+        }
+    },
+    {
+        titulo: 'Caderno',
+        preco: 20.00,
+        categoria: 'papelaria',
+        status: {
+            estoque: false,
+            ativo: false
+        }
+    },
+    {
+        titulo: 'Sorvete',
+        preco: 15.00,
+        categoria: 'alimento',
+        status: {
+            estoque: false,
+            ativo: true
+        }
+    },
+    {
+        titulo: 'Detergente',
+        preco: 7.0,
+        categoria: 'higiene',
+        status: {
+            estoque: true,
+            ativo: false
+        }
+    },
+]
+
+
+console.log(Produtos);
+
+//MAP 
+
+// console.log(Produtos.map(item=> `${item.titulo} por apenas: R$${item.preco.toFixed(2)}`));
+
+//FILTER 
+
+// console.log(Produtos.filter(item => item.preco>=15).filter(item => item['categoria'] == 'alimento'));
+
+//FIND
+
+// console.log(Produtos.find(item => item.preco < 5));
+// console.log("Index: "+ Produtos.findIndex(item => item.preco < 5));
+
+//REDUCE
+
+// console.log(Produtos.reduce((acc, p)=>{
+//     acc += p.preco;
+//     return acc;
+// },0))
+
+// console.log(Produtos.reduce((acc,p)=>{
+//     acc.push(p.titulo);
+//     return acc;
+// },[]))
+
+console.log(Produtos.reduce((acc,p)=>{
+    if(!acc[p.status.estoque]) acc[p.status.estoque] = [];
+    acc[p.status.estoque].push(p);
+    return acc
+},{}))
+
+// console.log(Produtos.reduce((acc,p)=>{
+//     acc += '\n' + '\n' + p.titulo
+//     return acc
+// }, ''))
+
+// function agrupar(parametro){
+//     return Produtos.reduce((acc,p)=>{
+//         if(!acc[p[parametro]]) acc[p[parametro]] = [];
+//         acc[p[parametro]].push(p);
+//         return acc
+//     },{})
+// }
+
---
+++ Node JS/Março/13-03/script1/desafio.js
@@ -0,0 +1,67 @@
+const fs = require('fs');
+const Pessoas = [
+    {
+        nome: "Guilherme",
+        idade: 20
+    },
+    {
+        nome: "Jessica",
+        idade: 30
+    },
+    {
+        nome: "Roseli",
+        idade: 52
+    },
+    {
+        nome: "Ivan",
+        idade: 57
+    },
+    {
+        nome: "Camilly",
+        idade: 19
+    },
+    {
+        nome: "Heloísa",
+        idade: 18
+    },
+    {
+        nome: "Matheus",
+        idade: 30
+    },
+]
+
+function deleteArq(name){
+    fs.unlink(name, (err)=>{
+        if(err) console.log(err);
+        else console.log(`Arquivo ${name} deletado com sucesso`);
+    })
+}
+
+let nomes = Pessoas.reduce((acc,p)=>{
+    acc += `${p.nome} ${p.idade} \n`;
+    return acc;
+},'')
+
+fs.writeFile('ListaDePessoas.csv', nomes, (err)=>{
+    if(err) console.log(err);
+    else console.log('Arquivo criado com sucesso!')
+})
+
+fs.readFile('ListaDePessoas.csv', 'utf-8', (err,data)=>{
+    if(err) console.log(err);
+    else{
+        fs.writeFile('Pessoas.txt', data.toLocaleUpperCase(), (error)=>{
+            if(error){
+                console.log(error);
+            }else{
+                console.log('Arquivo .txt criado com sucesso!');
+            }
+        })
+    }
+})
+
+setTimeout(()=>{
+    deleteArq('ListaDePessoas.csv');
+    deleteArq('Pessoas.txt')
+}, 8000);
+
