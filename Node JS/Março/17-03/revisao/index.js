b9e88a2cfcd725ad8e687646f30389429fefb2ea
Guilherme Bittelbrunn <guilherme.bitte@unifebe.edu.br>
2023-03-17 08:52:08 -0300

    add revisao 17-03
    Revendo conceitos dos pacotes FS e CSV, utilizando reduce e JSON para criação de arquivos com objetos

---
+++ Node JS/Março/17-03/revisao/index.js
@@ -0,0 +1,43 @@
+const csv = require('csv-parser');
+const fs = require('fs');
+const data_csv = [];
+
+fs.createReadStream('./exemplo.csv')
+.pipe(csv(
+    {
+    separator:','
+    }
+))
+.on('data', (data)=>{
+    data_csv.push(data);
+})
+.on('end', ()=>{
+    let lista_idade = data_csv.reduce((acc,p)=>{
+        let dec = Math.floor(parseInt(p['Age']) / 10);
+        if(!acc[dec + '0'] ){
+            acc[dec + '0']  = [];
+        }
+        acc[dec + '0'].push(p);
+        return acc
+    },{})
+    fs.writeFile('formatacao.txt', JSON.stringify(lista_idade), (err)=>{
+        if(err){
+            console.log(err);
+        }
+        else{
+            console.log('formatacao.txt criado com sucesso!');
+        }
+    })
+})
+.on('error', (error)=>{
+    console.log(error);
+})
+
+
+fs.readFile('./formatacao.txt', 'utf-8', (err,data)=>{
+    if(err){
+        console.log(err);
+    }else{
+        console.log(JSON.parse(data));
+    }
+})
