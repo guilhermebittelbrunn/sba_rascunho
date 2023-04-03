a2531fd55b6cae07d295260155be2f3837517a2e
Guilherme Bittelbrunn <guilherme.bitte@unifebe.edu.br>
2023-04-03 13:37:53 -0300

    jasper testes

--- Node JS/Desafios/Desafio teste/script.js
+++ Node JS/Desafios/Desafio teste/script.js
@@ -4,6 +4,7 @@ const jasper = require('node-jasper')({
     reports: {
         relatorio: {
             jasper: './reports/relatorio.jasper', //Doc.jasper path
+            jrxml: './reports/relatorio.jrxml', //Doc.jrxml path
             conn: 'in_memory_json',
         },
     },
@@ -11,14 +12,174 @@ const jasper = require('node-jasper')({
 
 const report = {
     report: 'relatorio', //File
-    data: {},
-    dataset: {
-        teste: ['Guilherme', 'Ivan'],
+    data: {
+        nome: 'Guilherme',
+        idade: 21,
+        turma: '1A',
     },
+    dataset: [
+        {
+            endereco: 'Rua ficticia, número inexistente, bairro imaginário',
+            telefone: '(00) 0000-0000',
+            Alunos: [
+                {
+                    nome: 'Guilherme',
+                    idade: 20,
+                    turma: '1A',
+                },
+                {
+                    nome: 'Ivan',
+                    idade: 18,
+                    turma: '3B',
+                },
+                {
+                    nome: 'Roseli',
+                    idade: 21,
+                    turma: '4A',
+                },
+                {
+                    nome: 'Jessica',
+                    idade: 17,
+                    turma: '2B',
+                },
+                {
+                    nome: 'Camilly',
+                    idade: 15,
+                    turma: '1B',
+                },
+                {
+                    nome: 'Guilherme',
+                    idade: 20,
+                    turma: '1A',
+                },
+                {
+                    nome: 'Ivan',
+                    idade: 18,
+                    turma: '3B',
+                },
+                {
+                    nome: 'Roseli',
+                    idade: 21,
+                    turma: '4A',
+                },
+                {
+                    nome: 'Jessica',
+                    idade: 17,
+                    turma: '2B',
+                },
+                {
+                    nome: 'Camilly',
+                    idade: 15,
+                    turma: '1B',
+                },
+                {
+                    nome: 'Guilherme',
+                    idade: 20,
+                    turma: '1A',
+                },
+                {
+                    nome: 'Ivan',
+                    idade: 18,
+                    turma: '3B',
+                },
+                {
+                    nome: 'Roseli',
+                    idade: 21,
+                    turma: '4A',
+                },
+                {
+                    nome: 'Jessica',
+                    idade: 17,
+                    turma: '2B',
+                },
+                {
+                    nome: 'Camilly',
+                    idade: 15,
+                    turma: '1B',
+                },
+                {
+                    nome: 'Guilherme',
+                    idade: 20,
+                    turma: '1A',
+                },
+                {
+                    nome: 'Ivan',
+                    idade: 18,
+                    turma: '3B',
+                },
+                {
+                    nome: 'Roseli',
+                    idade: 21,
+                    turma: '4A',
+                },
+                {
+                    nome: 'Jessica',
+                    idade: 17,
+                    turma: '2B',
+                },
+                {
+                    nome: 'Camilly',
+                    idade: 15,
+                    turma: '1B',
+                },
+                {
+                    nome: 'Guilherme',
+                    idade: 20,
+                    turma: '1A',
+                },
+                {
+                    nome: 'Ivan',
+                    idade: 18,
+                    turma: '3B',
+                },
+                {
+                    nome: 'Roseli',
+                    idade: 21,
+                    turma: '4A',
+                },
+                {
+                    nome: 'Jessica',
+                    idade: 17,
+                    turma: '2B',
+                },
+                {
+                    nome: 'Camilly',
+                    idade: 15,
+                    turma: '1B',
+                },
+                {
+                    nome: 'Guilherme',
+                    idade: 20,
+                    turma: '1A',
+                },
+                {
+                    nome: 'Ivan',
+                    idade: 18,
+                    turma: '3B',
+                },
+                {
+                    nome: 'Roseli',
+                    idade: 21,
+                    turma: '4A',
+                },
+                {
+                    nome: 'Jessica',
+                    idade: 17,
+                    turma: '2B',
+                },
+                {
+                    nome: 'Camilly',
+                    idade: 15,
+                    turma: '1B',
+                },
+            ],
+        },
+    ],
 };
 
 setTimeout(() => {
     let pdf = jasper.pdf(report);
     fs.writeFileSync('./teste.pdf', Buffer.from(pdf));
+    jasper.export(report, '');
     console.log('Arquivo criado');
 }, 1000);
