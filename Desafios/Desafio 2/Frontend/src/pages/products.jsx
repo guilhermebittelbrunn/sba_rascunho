76ee3d8372f97713c5343c2220b900d53e5f8b10
Guilherme Bittelbrunn <guilherme.bitte@unifebe.edu.br>
2023-04-28 11:15:55 -0300

    bug fix

--- Desafios/Desafio 2/Frontend/src/pages/products.jsx
+++ Desafios/Desafio 2/Frontend/src/pages/products.jsx
@@ -1,73 +1,171 @@
+// // import { Table } from 'antd';
+// import { useEffect, useState, useRef } from 'react';
+// import { useSelector } from 'react-redux';
+// import { SearchOutlined } from '@ant-design/icons';
+// import Highlighter from 'react-highlight-words';
+// import axios from 'axios';
+
 // import { Table } from 'antd';
-import { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
-import { SearchOutlined } from '@ant-design/icons';
-import { useRef, useState } from 'react';
-import Highlighter from 'react-highlight-words';
-import axios from 'axios';
+// const columns = [
+//   {
+//     title: 'Title',
+//     dataIndex: 'title',
+//     width: '30%',
+//   },
+//   {
+//     title: 'Description',
+//     dataIndex: 'description',
+//   },
+//   {
+//     title: 'Collection',
+//     dataIndex: 'collection',
+//     filters: [
+//       {
+//         text: '1A',
+//         value: '1A',
+//       },
+//       {
+//         text: '1B',
+//         value: '1B',
+//       },
+//       {
+//         text: '2A',
+//         value: '2A',
+//       },
+//       {
+//         text: '2B',
+//         value: '2B',
+//       },
+//       {
+//         text: '3A',
+//         value: '3A',
+//       },
+//       {
+//         text: '3B',
+//         value: '3B',
+//       },
+//     ],
+//     onFilter: (value, record) => record.collection.startsWith(value),
+//     width: '40%',
+//   },
+// ];
+
+
+// export default function Products(){
+//   const isLog = useSelector((login)=>{return login});
+//   const [data, setdata] = useState([]);
+
+//   async function findAlldata(){
+//     const {data} = await axios.get(`http://localhost:3000/product/${isLog.user.id}`);
+//     const list = data.map((item, key)=>{
+//       return {...item, ['key']: key}
+//     })
+//     setdata(list);
+//   }
+//   useEffect(()=>{
+//     isLog.status && findAlldata();
+//   },[]);
+
+//   return(
+//     <Table columns={columns} dataSource={data} />
+//   )
+// }
 
 import { Table } from 'antd';
 const columns = [
   {
-    title: 'Title',
-    dataIndex: 'title',
+    title: 'Name',
+    dataIndex: 'name',
+    filters: [
+      {
+        text: 'Joe',
+        value: 'Joe',
+      },
+      {
+        text: 'Category 1',
+        value: 'Category 1',
+        children: [
+          {
+            text: 'Yellow',
+            value: 'Yellow',
+          },
+          {
+            text: 'Pink',
+            value: 'Pink',
+          },
+        ],
+      },
+      {
+        text: 'Category 2',
+        value: 'Category 2',
+        children: [
+          {
+            text: 'Green',
+            value: 'Green',
+          },
+          {
+            text: 'Black',
+            value: 'Black',
+          },
+        ],
+      },
+    ],
+    filterMode: 'tree',
+    filterSearch: true,
+    onFilter: (value, record) => record.name.includes(value),
     width: '30%',
   },
   {
-    title: 'Description',
-    dataIndex: 'description',
+    title: 'Age',
+    dataIndex: 'age',
+    sorter: (a, b) => a.age - b.age,
   },
   {
-    title: 'Collection',
-    dataIndex: 'collection',
+    title: 'Address',
+    dataIndex: 'address',
     filters: [
       {
-        text: '1A',
-        value: '1A',
+        text: 'London',
+        value: 'London',
       },
       {
-        text: '1B',
-        value: '1B',
-      },
-      {
-        text: '2A',
-        value: '2A',
-      },
-      {
-        text: '2B',
-        value: '2B',
-      },
-      {
-        text: '3A',
-        value: '3A',
-      },
-      {
-        text: '3B',
-        value: '3B',
+        text: 'New York',
+        value: 'New York',
       },
     ],
-    onFilter: (value, record) => record.collection.startsWith(value),
+    onFilter: (value, record) => record.address.startsWith(value),
+    filterSearch: true,
     width: '40%',
   },
 ];
-
-
-export default function Products(){
-  const isLog = useSelector((login)=>{return login});
-  const [data, setdata] = useState([]);
-
-  async function findAlldata(){
-    const {data} = await axios.get(`http://localhost:3000/product/${isLog.user.id}`);
-    const list = data.map((item, key)=>{
-      return {...item, ['key']: key}
-    })
-    setdata(list);
-  }
-  useEffect(()=>{
-    isLog.status && findAlldata();
-  },[]);
-
-  return(
-    <Table columns={columns} dataSource={data} />
-  )
-}
+const data = [
+  {
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sydney No. 1 Lake Park',
+  },
+  {
+    key: '4',
+    name: 'Jim Red',
+    age: 32,
+    address: 'London No. 2 Lake Park',
+  },
+];
+const onChange = (pagination, filters, sorter, extra) => {
+  console.log('params', pagination, filters, sorter, extra);
+};
+const App = () => <Table columns={columns} dataSource={data} onChange={onChange} />;
+export default App;
