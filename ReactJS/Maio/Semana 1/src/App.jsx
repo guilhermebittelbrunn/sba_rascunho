1b2b461d405dab04d5de59b5a843ce1c46708a5e
Guilherme Bittelbrunn <guilherme.bitte@unifebe.edu.br>
2023-05-04 17:28:10 -0300

    add useRef and useForm

--- ReactJS/Maio/Semana 1/src/App.jsx
+++ ReactJS/Maio/Semana 1/src/App.jsx
@@ -1,21 +1,35 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef, useCallback } from 'react'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import './App.css'
+import axios from 'axios';
 
 function App() {
-
+  const ref = useRef();
   const [count, setCount] = useState(0);
+  const [pokemons, setPokemons] = useState([]); 
+  const [loading, setLoading] = useState(false);
+  
+  useEffect(()=>{
+    // setLoading(true);
+    // (async()=>{
+    //   for(let i = 1; i < 100; i++){
+    //     const {data} = await axios.get(`https://pokeapi.co/api/v2/pokemon/${i}`);
+    //     setPokemons(preventValue => [...preventValue, data]);
+    //   }
+    //   setLoading(false)
+    // })()
+  },[])
 
   return (
     <>
     
-          <h2>Home page</h2>
+          <h2 ref={ref}>Home page</h2>
           <h3>state clicked {count} times</h3>
-          <button onClick={()=>{setCount(preventValue=>preventValue + 1)}}>Increment state</button>
+          <button onClick={()=>{setCount(preventValue=>preventValue + 1, ref.current.style = 'display: none')}}>Increment state</button>
           <br></br>
           <Link to={'/new'}>Criar</Link>
           <Link to={`/new/${count}`}>Editar</Link>
-  
+          {/* {!loading ? pokemons.map((pokemon, k) => <p key={k}>{pokemon.name}</p>) : <h4>Carregando...</h4>} */}
     </>
   )
 }
--- ReactJS/Maio/Semana 1/src/components/Form.jsx
+++ ReactJS/Maio/Semana 1/src/components/Form.jsx
@@ -1,18 +1,33 @@
+import { useEffect, useState , useRef} from "react";
 import { useParams, useLocation, Link } from "react-router-dom"
+import { useForm } from "react-hook-form";
 
 export default function Form(){
     const {id} = useParams();
-    const location = useLocation();
+    const [text,setText] = useState({});
+    const count = useRef(0);
+    const {register, handleSubmit} = useForm();
+    
+    const handleChange = (e)=>{
+       setText((preventValue)=>{
+        return {...preventValue, [e.target.name] : e.target.value}
+       })
+    }
+
+    const addUser = (data) => console.log(data, text);
+
+    useEffect(()=>{
+        console.log(text);
+    }, [text])
+
+
     return(
         <>
-            <form>
+            <form onSubmit={handleSubmit(addUser)}>
                 <h3>{id ? 'Update' : 'Create'}</h3>
-                <input type="text" name="name" id="name" placeholder="name" value={id}/>
-                <input type="password" name="password" id="password" placeholder="password"/>
-                <button onClick={(e)=>{
-                    e.preventDefault();
-                    id ? alert('Update') : alert('Create');
-                }}>Send</button>,
+                <input type="text" name="name" id="name" placeholder="name" value={id} {...register('name')} onChange={handleChange} />
+                <input type="password" name="password" id="password" placeholder="password" {...register('password')} onChange={handleChange} />
+                <button> Send</button>
             </form>
             <Link to={'/'}>Go to home</Link>
         </>
